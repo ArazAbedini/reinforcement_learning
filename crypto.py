4cccf8eda9c818dccf85ffd150089db4fd3ddc73
ArazAbedini <araz.abedini1381@gmail.com>
2024-05-11 11:20:05 +0330

    Revert "Revert "initial models states""
    This reverts commit 486cebd54f7004c61a5333121f7a3768a7b30f13.

--- crypto.py
+++ crypto.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import ast
+from model import Model
 import gym
 
 
 env = gym.make('CartPole-v1', render_mode='rgb_array')
 obs, info = env.reset(seed=42)
 
-FILE_PATH = '/home/araz/Documents/ai/files/AVAX_1h.txt'
-with open(FILE_PATH, 'r') as file:
-    content = file.read()
-content_list = ast.literal_eval(content)
+
+
+
+FILE_PATH = 'data/AVAX_1h.txt'
+model = Model(FILE_PATH)
+# print(model.df)
+model.plot_df()
+
+
